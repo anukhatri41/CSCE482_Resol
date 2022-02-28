877f5d24c8021f72062c329e4800c7d01517f8e1
gtugwell <gtugwell@tamu.edu>
2022-02-27 20:54:42 -0600

    Setting up TransactionTestCode directory, changing structure of folders.

---
+++ TransactionTestCode/client/transaction.ts
@@ -0,0 +1,13 @@
+import * as web3 from '@solana/web3.js';
+import {
+    Keypair,
+    Connection,
+    PublicKey,
+    LAMPORTS_PER_SOL,
+    SystemProgram,
+    TransactionInstruction,
+    Transaction,
+    sendAndConfirmTransaction,
+} from '@solana/web3.js';
+import base58 from 'bs58';
+

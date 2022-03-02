b3563a61630d2255bbcdc35eb653255e85aa3818
gtugwell <gtugwell@tamu.edu>
2022-03-02 12:24:38 -0600

    Working on sending instruction through solana blockchain instead of an sdk

--- TransactionTestCode/client/exchangeClient.ts
+++ TransactionTestCode/client/exchangeClient.ts
@@ -33,7 +33,7 @@ const amount = 0.01;
 (async () => {
     // Establishing Market Connect
     console.log("Connecting to cluster: ", ChosenCluster)
-    let connection = new web3.Connection(ChosenCluster);
+    let connection = new web3.Connection(ChosenCluster, 'confirmed');
     let marketAddress = new web3.PublicKey(details.RAYDIUM_SOL_USDC);
     let programAddress = new web3.PublicKey(details.SERUM_PROGRAM_ID);
 
@@ -41,11 +41,11 @@ const amount = 0.01;
     //let programPubkey = ray.publicKey(details.SERUM_PROGRAM_ID);
     //let marketPubkey = ray.publicKey(details.RAYDIUM_SOL_USDC);
 
-    let market = new ray.Market();
-    market = await ray.Market.getAssociatedAuthority({
-        programId: programAddress,
-        marketId: marketAddress
-    });
+    // let market = new ray.Market();
+    // market = await ray.Market.getAssociatedAuthority({
+    //     programId: programAddress,
+    //     marketId: marketAddress
+    // });
     console.log("Market Connecting to: ", market);
     //let market = await Market.load(connection, marketAddress, {}, programAddress);
     console.log("Connected...");
@@ -56,48 +56,71 @@ const amount = 0.01;
 
     let RouteInfo = ["amm", "serum", "route"];
     console.log("Established Route Info");
-    let solToken = new ray.Token('EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v', 1);
-    let solTokenAmount = await new ray.TokenAmount(solToken, 1);
+    // let solToken = new ray.Token('So11111111111111111111111111111111111111112', 1);
+    // let solTokenAmount = await new ray.TokenAmount(solToken, 1);
     console.log("Established sol");
     //let sol = ray.Token.SOL;
     let per = new ray.Percent(1,2);
     console.log("Declared per");
-    //console.log("POOL INFO", ray.MAINNET_OFFICIAL_LIQUIDITY_POOLS)
-    //let amm = ray.AmmSource ()
+    // console.log("POOL INFO", ray.MAINNET_OFFICIAL_LIQUIDITY_POOLS)
+    // //let amm = ray.AmmSource ()
     // Code works up to here...
     
     console.log("Creating serum source");
+    const keys = [
+        { pubkey: marketAddress, isSigner: false, isWritable: true },
+        { pubkey: openOrders, isSigner: false, isWritable: true },
+        { pubkey: requestQueue, isSigner: false, isWritable: true },
+        { pubkey: payer, isSigner: false, isWritable: true },
+        { pubkey: owner, isSigner: true, isWritable: false },
+        { pubkey: baseVault, isSigner: false, isWritable: true },
+        { pubkey: quoteVault, isSigner: false, isWritable: true },
+        { pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false },
+        { pubkey: SYSVAR_RENT_PUBKEY, isSigner: false, isWritable: false },
+      ];
+
+    const instruction = new web3.TransactionInstruction({
+        keys,
+        programId,
+        data: encodeInstruction({
+          newOrder: clientId
+            ? { side, limitPrice, maxQuantity, orderType, clientId }
+            : { side, limitPrice, maxQuantity, orderType },
+        }),
+    });
+    const signature: string = await web3.sendAndConfirmTransaction(
+        connection,
+        new web3.Transaction().add(instruction),
+        [sender],
+    );
+      console.log("tx: ", signature);
+
     // ray.Trade.getBestAmountIn({
-    //     //markets: SerumSource({
-    //     //     marketKeys: [marketAddress],
-    //     //     bids: [],
-    //     //     asks: []
-    //     // }),
-    //     // pools: ray.MAINNET_OFFICIAL_LIQUIDITY_POOLS[0], 
-    //     // amountOut: solTokenAmount,
-    //     // currencyIn: ray.Token.SOL,
-    //     // slippage: per
-    //     // markets: market,
-    //     // amountOut: sol, // Maybe SOL as well???
-    //     // currencyIn: ray.Token.SOL, // SOL
-    //     // slippage: per // 50% slippage?
+    //     pools: ray.MAINNET_OFFICIAL_LIQUIDITY_POOLS[0], 
+    //     amountOut: solTokenAmount,
+    //     currencyIn: ray.Token.SOL,
+    //     slippage: per
+    //     markets: market,
+    //     amountOut: sol, // Maybe SOL as well???
+    //     currencyIn: ray.Token.SOL, // SOL
+    //     slippage: per // 50% slippage?
     // });
 
-    let solTokenAccount:TokenAccount[];
-    solTokenAccount.push([solToken]);
+    // let solTokenAccount:TokenAccount[];
+    // solTokenAccount.push(solTokenAmount);
 
-    ray.Trade.makeTradeTransaction({
-        connection: connection,
-        routes: RouteInfo[0],
-        routeType: RouteType,
-        userKeys: {
-            tokenAccounts: solTokenAccount,
-            owner: sender.publicKey
-        },
-        amountIn: solTokenAmount,
-        amountOut: solTokenAmount,
-        fixedSide: "in"
-    });
+    // ray.Trade.makeTradeTransaction({
+    //     connection: connection,
+    //     routes: RouteInfo[0],
+    //     routeType: RouteType,
+    //     userKeys: {
+    //         tokenAccounts: solTokenAccount,
+    //         owner: sender.publicKey
+    //     },
+    //     amountIn: solTokenAmount,
+    //     amountOut: solTokenAmount,
+    //     fixedSide: "in"
+    // });
 
     // let buy = new Trade();
     // buy = ray.Trade.makeTradeTransaction({

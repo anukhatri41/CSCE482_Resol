e41297018fa1baa0a6e047d4ebca8ee94fe6e74c
gtugwell <gtugwell@tamu.edu>
2022-03-04 13:52:42 -0600

    Added some working code, we are now able to buy from Orca.

---
+++ TransactionTestCode/client/buyingCode.ts
@@ -0,0 +1,61 @@
+import { readFile } from "mz/fs";
+import { Connection, Keypair } from "@solana/web3.js";
+import { getOrca, OrcaPoolConfig } from "@orca-so/sdk";
+import Decimal from "decimal.js";
+const sleep = require('./sleep');
+
+const main = async () => {
+  // Getting keypair
+  const secretKeyString = await readFile("/Users/gtugwell/.config/solana/id.json", {
+    encoding: "utf8",
+  });
+  const secretKey = Uint8Array.from(JSON.parse(secretKeyString));
+  const owner = Keypair.fromSecretKey(secretKey);
+
+  // Cluster definitions
+  const devnet = 'https://api.devnet.solana.com';
+  const mainnet = 'https://api.mainnet-beta.solana.com';
+  const serumAPI = 'https://solana-api.projectserum.com';
+
+  // Connecting to cluster & then initializing orca connection
+  const connection = new Connection(mainnet, "singleGossip");
+  const orca = getOrca(connection);
+
+  try {
+    // Amount of SOL per transaction
+    const solAmount = new Decimal(0.005);
+
+    // Getting SOL USDC Orca pool info
+    const orcaSolPool = orca.getPool(OrcaPoolConfig.SOL_USDC);
+    const solToken = orcaSolPool.getTokenA();
+    const usdcToken = orcaSolPool.getTokenB();
+    
+    for (let i = 0; i < 5; i++) {
+        console.log("Making transaction #", i);
+
+        const quote = await orcaSolPool.getQuote(solToken, solAmount);
+        const usdcAmount = quote.getMinOutputAmount();
+
+        console.log(`Swap ${solAmount.toString()} SOL for at least ${usdcAmount.toNumber()} USDC`);
+        const swapPayload = await orcaSolPool.swap(owner, solToken, solAmount, usdcAmount);
+        const swapTxId = await swapPayload.execute();
+        console.log("Swapped:", swapTxId, "\n");
+
+        // Sleeping for 10 seconds
+        console.log("Going to sleep for 10 seconds.");
+        await sleep(10000);
+        console.log("I have awakened.");
+    }
+
+  } catch (err) {
+    console.warn(err);
+  }
+};
+
+main()
+  .then(() => {
+    console.log("Done");
+  })
+  .catch((e) => {
+    console.error(e);
+  });
---
+++ TransactionTestCode/client/orcaPractice.ts
@@ -0,0 +1,64 @@
+import { readFile } from "mz/fs";
+import { Connection, Keypair } from "@solana/web3.js";
+import { getOrca, OrcaFarmConfig, OrcaPoolConfig } from "@orca-so/sdk";
+//import { getOrca, OrcaFarmConfig, OrcaPoolConfig, Network } from "@orca-so/sdk";
+import Decimal from "decimal.js";
+
+const main = async () => {
+  /*** Setup ***/
+  // 1. Read secret key file to get owner keypair
+  const secretKeyString = await readFile("/Users/gtugwell/.config/solana/id.json", {
+    encoding: "utf8",
+  });
+  const secretKey = Uint8Array.from(JSON.parse(secretKeyString));
+  const owner = Keypair.fromSecretKey(secretKey);
+
+  const devnet = 'https://api.devnet.solana.com';
+  const mainnet = 'https://api.mainnet-beta.solana.com';
+  const serumAPI = 'https://solana-api.projectserum.com';
+
+  // 2. Initialzie Orca object with mainnet connection
+  const connection = new Connection(mainnet, "singleGossip");
+  const orca = getOrca(connection);
+// const connection = new Connection("https://api.devnet.solana.com", "singleGossip");
+// const orca = getOrca(connection, Network.DEVNET);
+
+  try {
+    /*** Swap ***/
+    // 3. We will be swapping 0.1 SOL for some ORCA
+    // const orcaSolPool = orca.getPool(OrcaPoolConfig.ORCA_SOL);
+    // const solToken = orcaSolPool.getTokenB();
+    // const solAmount = new Decimal(0.1);
+    // const quote = await orcaSolPool.getQuote(solToken, solAmount);
+    // const orcaAmount = quote.getMinOutputAmount();
+
+    // console.log(`Swap ${solAmount.toString()} SOL for at least ${orcaAmount.toNumber()} ORCA`);
+    // const swapPayload = await orcaSolPool.swap(owner, solToken, solAmount, orcaAmount);
+    // const swapTxId = await swapPayload.execute();
+    // console.log("Swapped:", swapTxId, "\n");
+    
+    // Modifying code to swap SOL for USDC
+    const orcaSolPool = orca.getPool(OrcaPoolConfig.SOL_USDC);
+    const solToken = orcaSolPool.getTokenA();
+    const usdcToken = orcaSolPool.getTokenB();
+    const solAmount = new Decimal(0.1);
+    const quote = await orcaSolPool.getQuote(solToken, solAmount);
+    const usdcAmount = quote.getMinOutputAmount();
+
+    console.log(`Swap ${solAmount.toString()} SOL for at least ${usdcAmount.toNumber()} USDC`);
+    const swapPayload = await orcaSolPool.swap(owner, solToken, solAmount, usdcAmount);
+    const swapTxId = await swapPayload.execute();
+    console.log("Swapped:", swapTxId, "\n");
+
+  } catch (err) {
+    console.warn(err);
+  }
+};
+
+main()
+  .then(() => {
+    console.log("Done");
+  })
+  .catch((e) => {
+    console.error(e);
+  });

591a81d037708875b9b8f13d11ebc177f70126f3
gtugwell <gtugwell@tamu.edu>
2022-03-28 13:21:11 -0500

    Adding test code

--- src/client/testCode.ts
+++ src/client/testCode.ts
@@ -1,4 +1,4 @@
-import { Connection, Keypair, LAMPORTS_PER_SOL, PublicKey, TokenAccountsFilter } from "@solana/web3.js";
+import { Connection, Keypair, LAMPORTS_PER_SOL, PublicKey, sendAndConfirmTransaction, TokenAccountsFilter } from "@solana/web3.js";
 import { executeOrcaSwap, getOrcaQuote } from "./utils/orcaSwap";
 import { executeJupiterSwap, retrieveJupRoutes } from "./utils/jupiterSwap";
 import { fetchWalletBalance } from "./utils/shared";
@@ -8,8 +8,51 @@ import {
 } from "./constants";
 const sleep = require('./sleep');
 
+const routeOutput = async () => {
+
+  require('dotenv').config()
+  const details = {
+      sender_keypair: process.env.SENDER_KEY as string,
+      secret: process.env.SENDER_SECRET as string,
+      reciever: process.env.DEFAULT_RECEIVER_PUBKEY as string,
+      _RPC: process.env.RPC_ENDPOINT as string, // named _RPC because functions were throwing a fit when passing in details.RPC
+  };
+
+  // if secret key is in .env:
+  const WALLET_PRIVATE_KEY = details.secret
+  const USER_PRIVATE_KEY = bs58.decode(WALLET_PRIVATE_KEY);
+  const owner = Keypair.fromSecretKey(USER_PRIVATE_KEY);
+
+  const RPC = details._RPC;
+  const devnet = 'https://api.devnet.solana.com';
+  const mainnet = 'https://api.mainnet-beta.solana.com';
+  const serumAPI = 'https://solana-api.projectserum.com';
+
+  // 2. Initialize Orca object with mainnet connection
+  const connection = new Connection(RPC);
+
+  // Info
+  let inAmount = 0.05;
+  let tokenIn = 'SOL';
+  let tokenOut = 'OXY';
+
+  try {
+  const route = await retrieveJupRoutes({connection, inAmount, owner, tokenIn, tokenOut});
+
+  console.log(route!.routesInfos[0].marketInfos[0].marketMeta);
+
+  
+  // const signature: string = await sendAndConfirmTransaction(connection, swapPayload.transaction, swapPayload.signers);
+  // console.log("tx: ", signature);
+  } catch(err) {
+    console.warn(err);
+  }
+}
+
 const main = async () => {
     
+  await routeOutput();
+
   };
     main()
       .then(() => {

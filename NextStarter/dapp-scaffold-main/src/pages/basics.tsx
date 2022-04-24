37895320e72c5aa574be54bef0e3bd3828765815
robertelizondo <robmelizondo@gmail.com>
2022-04-24 11:39:04 -0500

    fixed label cut off

--- NextStarter/dapp-scaffold-main/src/pages/basics.tsx
+++ NextStarter/dapp-scaffold-main/src/pages/basics.tsx
@@ -117,7 +117,7 @@ function Basics ({balanceData}) {
       <div className="md:hero mx-auto p-4">
 
     <div className="md:hero-content flex flex-col">
-      <h1 className="text-center text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-tr from-[#9945FF] to-[#14F195]">
+      <h1 style={{padding: "5px"}}  className="text-center text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-tr from-[#9945FF] to-[#14F195]">
         Analytics
       </h1>
 
--- NextStarter/dapp-scaffold-main/src/pages/trading_2.tsx
+++ NextStarter/dapp-scaffold-main/src/pages/trading_2.tsx
@@ -112,7 +112,7 @@ class Trading2 extends React.Component {
               <div className="md:hero mx-auto p-4">
         
             <div className="md:hero-content flex flex-col">
-              <h1 className="text-center text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-tr from-[#9945FF] to-[#14F195]">
+              <h1 style={{padding: "5px"}} className="text-center text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-tr from-[#9945FF] to-[#14F195]">
                 Trading currently running
               </h1>
 

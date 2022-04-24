03e2a718f14c7de7572751924a390c5e23e2f3ec
gtugwell <gtugwell@tamu.edu>
2022-04-24 17:05:45 -0500

    exec server

--- NextStarter/dapp-scaffold-main/src/pages/index.tsx
+++ NextStarter/dapp-scaffold-main/src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { exec } from "child_process";
 import type { NextPage } from "next";
 import Head from "next/head";
 import { HomeView } from "../views";
@@ -20,9 +21,23 @@ const Home: NextPage = (props) => {
 
 export default Home;
 
+// var express = require('express');
+// var jsonServer = require('json-server');
+
+// var server = express();
+// // ...
+
+// // You may want to mount JSON Server on a specific end-point, for example /api
+// // Optiona,l except if you want to have JSON Server defaults
+// // server.use('/api', jsonServer.defaults()); 
+// server.use(jsonServer.router('db.json'));
+
+// server.listen(4000);
+
 
 export async function getServerSideProps(){
   console.log("howdy--------");
+  exec("yarn run json-server --watch db.json --port 4000")
   await axios.put('http://localhost:4000/graph_data/1', {
           txId: null,
           totalBalance: 0,
